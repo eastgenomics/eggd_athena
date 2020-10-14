8ac7d459deb866881a9831cbf65f0e0ec9eaf166
jethror1 <rainford1995@gmail.com>
2020-10-14 17:01:00 +0100

    changes to use local packages

--- src/eggd_athena.sh
+++ src/eggd_athena.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # eggd_athena 1.0.0
 
+set -exo pipefail
+
 main() {
 
     # download inputs
@@ -9,19 +11,17 @@ main() {
     dx download "$pb_bed"
 
     if [ ${build+x} ]; then
-        echo "BUILD FILE PASSED"
         dx download "$build"
     fi
 
     # download SNPs if given to SNPs dir
     mkdir snps && cd snps
-
+    
     for i in "${!snps[@]}"
     do
         dx download "${snps[$i]}"
     done
-    
-    ls
+
     cd ~/
 
     # set up bedtools
@@ -35,21 +35,27 @@ main() {
     bash ~/Miniconda3-latest-Linux-x86_64.sh -b
 
     # unzip athena and install requirements
-    # unzip athena-master.zip
-    # sudo chmod -R 775 athena
-    # ./miniconda3/bin/pip install -r athena/requirements.txt
+    # should include .zip of athena from releases
+    unzip athena-*.zip
+    sudo chmod -R 775 athena-*
+    
+    # install required python packages from local packages dir
+    echo "Installing python packages"
+    cd packages
+    ~/miniconda3/bin/pip install -q certifi-* pytz-* python_dateutil-* pysam-* cycler-* kiwisolver-* Pillow* \
+        retrying-* pyparsing-* numpy-* SQLAlchemy-* pandas-* pandasql-* matplotlib-* plotly-* pybedtools-*
+    cd ~
 
-    # DEVELOPMENT - unzip athena-devlopment and install requirements
-    unzip athena-development.zip
-    sudo chmod -R 775 athena-development
-    ./miniconda3/bin/pip install -qr athena-development/requirements.txt
-
-    echo "Finished setup."
+    echo "Finished setup. Beginning analysis."
+    echo "Annotating bed file."
 
     # annotate bed file
     bash ./athena-development/bin/annotate_bed.sh -i "$panel_bed_name" -g "$exons_nirvana_name" -b "$pb_bed_name"
-    ls
-    annotated_bed=$(ls ./*_annotated.bed)
+    annotated_bed=$(find . -name "*_annotated.bed")
+
+    echo "name $name"
+    echo "build $buildname"
+    echo "thresholds $thresholds"
 
     # if sample naming given replace spaces with "_"
     if [ "$name" ]; then name=${name// /_}; fi
@@ -63,33 +69,34 @@ main() {
     
     stats_cmd="--file $annotated_bed"
     stats_cmd+=$stats_args
-    echo "stats cmd" $stats_cmd
+    echo "Generating coverage stats with: " $stats_cmd
     
     # generate single sample stats
-    ./miniconda3/bin/python ./athena-development/bin/coverage_stats_single.py $stats_cmd
+    time ./miniconda3/bin/python ./athena-development/bin/coverage_stats_single.py $stats_cmd
         
-    exon_stats=$(ls ./athena-development/output/*exon_stats.tsv)
-    gene_stats=$(ls ./athena-development/output/*gene_stats.tsv)
+    exon_stats=$(find ./athena-development/output/ -name "*exon_stats.tsv")
+    gene_stats=$(find ./athena-development/output/ -name "*gene_stats.tsv")
 
     # build string of inputs for report script
     report_args=""
 
     if [ "$cutoff_threshold" ]; then report_args+=" --threshold $cutoff_threshold"; fi
     if [ "$name" ]; then report_args+=" --sample_name $name"; fi
+    if [ "$panel" = true ]; then report_args+=" --panel $panel_bed_name"; fi
     if [ "${!snps[@]}" ]; then 
         snp_vcfs=$(find ~/snps/ -name "*.vcf")
         echo $snp_vcfs
         report_args+=" --snps $snp_vcfs";
     fi
 
-    report_cmd="./athena-development/bin/coverage_report_single.py --exon_stats $exon_stats --gene_stats $gene_stats --raw_coverage $annotated_bed"
+    report_cmd="./athena-development/bin/coverage_report_single.py --exon_stats $exon_stats --gene_stats $gene_stats --raw_coverage $annotated_bed --limit $limit"
     report_cmd+=$report_args
-    echo "report cmd" $report_cmd
+    echo "Generating report with: " $report_cmd
 
     # generate report
-    ./miniconda3/bin/python $report_cmd
+    time ./miniconda3/bin/python $report_cmd
     
-    report=$(ls ./athena-development/output/*coverage_report.html)
+    report=$(find ./athena-development/output/ -name "*coverage_report.html")
 
     echo "Completed. Uploading files"
 
